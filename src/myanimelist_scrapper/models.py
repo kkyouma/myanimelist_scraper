abc326d8edb2b2795f9b7d85c2c948b9860e6e26
kkyouma <144501527+kkyouma@users.noreply.github.com>
2025-01-11 17:24:22 -0300

    add list of media items and scrap for stats

--- src/myanimelist_scrapper/models.py
+++ src/myanimelist_scrapper/models.py
@@ -1,28 +1,35 @@
 from dataclasses import dataclass
+from datetime import date
 from urllib.parse import urljoin
 
 from bs4.element import BeautifulSoup
 
 
+@dataclass
 class Media:
     """Base class for media items."""
 
     name: str
     url: str
-    media_type: str 
+    media_type: str | None
+    status: str | None
+    description: str | None
+    generes: list[str] | None
+    themes: list[str] | None
+    authors: str | list[str]
+    demographic: list[str] | str | None
+    score: float | None
+    rank: int | None
+    popularity: int | None
+    members: int | None
 
     @property
     def stats_url(self) -> str:
         """Get url for the media stats (metadata)."""
         return urljoin(self.url, "stats")
 
-    @property
-    def characters_url(self) -> str:
-        """Get url for the media characters."""
-        return urljoin(self.url, "characters")
-
     @classmethod
-    def from_soup_tag(cls, link_tag: BeautifulSoup):
+    def from_soup_tag(cls, link_tag: BeautifulSoup) -> Media:
         """Create a Manga instance from a BeautifulSoup tag."""
         return cls(
             name=link_tag.text.strip(),
@@ -35,14 +42,23 @@ class Manga(Media):
     """Represesnts a manga entry with its metadata."""
 
     media_type = "Manga"
+    published: dict[str, date]
+    volumes: int | str
+    chapters: int | str
+    serialization: str
 
 
 class Anime(Media):
     """Represents a anime entry with its metadata."""
 
     media_type = "Anime"
-
-    @property
-    def episodes():
-        pass
-
+    episodes: int | str
+    aired: dict[str, date]
+    premiered: date  # NOTE: change type if datetime dont support "fall 2023" format
+    brodcast: date # %A at %H:%M (%Z)
+    producers: list[str]
+    licensors: str | list[str]
+    studios: str | list[str]
+    source: str
+    duration: int  # Duration in minutes
+    rating: str
--- src/myanimelist_scrapper/scrapper.py
+++ src/myanimelist_scrapper/scrapper.py
@@ -1,11 +1,13 @@
+import time
 from dataclasses import dataclass
 from enum import Enum
 from typing import ClassVar
+from urllib.parse import parse_qs, urlparse
 
 import requests
 from bs4 import BeautifulSoup
 
-from myanimelist_scrapper.models import Manga
+from myanimelist_scrapper.models import Media
 from myanimelist_scrapper.storage import Storage
 
 
@@ -20,6 +22,7 @@ class MediaConfig:
 
     base_url: str
     items_per_page: int = 50
+    rate_limit: float = 1.0  # Time to wait between request in seconds
 
 
 class MediaScraper:
@@ -36,27 +39,68 @@ class MediaScraper:
         self.config = self.CONFIGS[media_type]
         self.media_type = media_type
 
-    def get_page_url(self, top_n: int) -> str:
+        # Set up session headers to be more browser-like
+        self.session.headers.update(
+            {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+            },
+        )
+
+    def get_page_url(self, page: int = 1) -> str | None:
         """Generate URL for a specific page."""
-        return f"{self.config.base_url}?limit={top_n}"
+        offset = (page - 1) * self.config.items_per_page
+        return f"{self.config.base_url}?limit={offset}"
 
-    def extract_items(self, html_content: str) -> list[Media]:
-        """Extract"""
+    def fetch_page(
+        self,
+        url: str,
+        retries: int = 3,
+        timeout: int = 5,
+    ) -> str | None:
+        """Fetch page content retry logic."""
+        for attempt in range(retries):
+            try:
+                time.sleep(self.config.rate_limit)
 
-    def fetch_page(self, url: str, timeout: int = 5) -> str:
-        """Fetch and save html."""
-        response = self.session.get(url, timeout=timeout)
-        response.raise_for_status()
+                response = self.session.get(url, timeout=timeout)
+                response.raise_for_status()
 
-        filename = f"{url}.html"
-        self.storage.save_html(response.text, filename)
+            except requests.RequestException as e:
+                if attempt == retries - 1:
+                    print(f"Failed to fetch {url} after {retries} attempts: {e}")
+                time.sleep(2**attempt)
 
-        return response.text
+            else:
+                return response.text
+        return None
 
-    def extract_manga(self, html_content: str) -> list[Manga]:
+    # def save_file(self, content: str, url: str | None) -> str | None:
+    #     """Save page content to storage if storage is configured."""
+    #     if not self.storage:
+    #         return None
+    #
+    #     parsed_url = urlparse(url)
+    #     params = parse_qs(parsed_url.query)
+    #     limit = params.get("limit", ["0"])[0]
+    #
+    #     if "stats" in url:
+    #         media_id = url.split("/")[-2]
+    #         filename = f"{self.media_type.value}_stats_{media_id}.html"
+    #
+    #     else:
+    #         filename = f"{self.media_type.value}_stats_{limit or 'default'}.html"
+    #
+    #     return self.storage.save_html(content, filename)
+
+    def extract_stats(self, url: str) -> dict[str, int | str | list[str] | None]:
+        content = self.fetch_page(url)
+
+    def extract_media(html_content: str) -> list[Manga]:
         """Parse HTML and extract manga metadata."""
         soup = BeautifulSoup(html_content, "html.parser")
 
         selector = "a[class*='fs14 fw-b']"
 
-        return [Manga.from_soup_tag(manga_item) for manga_item in soup.select(selector)]
+        return [Media.from_soup_tag(media) for media in soup.select(selector)]
