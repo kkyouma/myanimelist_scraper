68b03e9effee387d011f9012ac1a51f11b5932e5
kkyouma <144501527+kkyouma@users.noreply.github.com>
2025-01-11 02:41:36 -0300

    change element and classes

--- src/myanimelist_scrapper/models.py
+++ src/myanimelist_scrapper/models.py
@@ -4,17 +4,22 @@ from urllib.parse import urljoin
 from bs4.element import BeautifulSoup
 
 
-@dataclass
-class Manga:
-    """Represesnts a manga entry with its metadata."""
+class Media:
+    """Base class for media items."""
 
     name: str
     url: str
+    media_type: str 
 
     @property
     def stats_url(self) -> str:
-        """Get the stats page URL."""
+        """Get url for the media stats (metadata)."""
         return urljoin(self.url, "stats")
+
+    @property
+    def characters_url(self) -> str:
+        """Get url for the media characters."""
+        return urljoin(self.url, "characters")
 
     @classmethod
     def from_soup_tag(cls, link_tag: BeautifulSoup):
@@ -23,3 +28,21 @@ class Manga:
             name=link_tag.text.strip(),
             url=link_tag.get("href"),
         )
+
+
+@dataclass
+class Manga(Media):
+    """Represesnts a manga entry with its metadata."""
+
+    media_type = "Manga"
+
+
+class Anime(Media):
+    """Represents a anime entry with its metadata."""
+
+    media_type = "Anime"
+
+    @property
+    def episodes():
+        pass
+
--- src/myanimelist_scrapper/scrapper.py
+++ src/myanimelist_scrapper/scrapper.py
@@ -1,19 +1,47 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import ClassVar
+
 import requests
 from bs4 import BeautifulSoup
 
 from myanimelist_scrapper.models import Manga
 from myanimelist_scrapper.storage import Storage
 
-MANGA_LINK = "https://myanimelist.net/topmanga.php"
-ANIME_LINK = "https://myanimelist.net/topanime.php"
+
+class MediaType(Enum):
+    MANGA = "manga"
+    ANIME = "anime"
 
 
-class MangaScraper:
+@dataclass
+class MediaConfig:
+    """Configuration for different media types."""
+
+    base_url: str
+    items_per_page: int = 50
+
+
+class MediaScraper:
     """Main scraper class for extracting manga information."""
 
-    def __init__(self, storage: Storage) -> None:
+    CONFIGS: ClassVar = {
+        MediaType.MANGA: MediaConfig("https://myanimelist.net/topmanga.php"),
+        MediaType.ANIME: MediaConfig("https://myanimelist.net/topanime.php"),
+    }
+
+    def __init__(self, storage: Storage, media_type: MediaType) -> None:
         self.storage = storage
         self.session = requests.Session()  # Use same conection
+        self.config = self.CONFIGS[media_type]
+        self.media_type = media_type
+
+    def get_page_url(self, top_n: int) -> str:
+        """Generate URL for a specific page."""
+        return f"{self.config.base_url}?limit={top_n}"
+
+    def extract_items(self, html_content: str) -> list[Media]:
+        """Extract"""
 
     def fetch_page(self, url: str, timeout: int = 5) -> str:
         """Fetch and save html."""
@@ -28,9 +56,7 @@ class MangaScraper:
     def extract_manga(self, html_content: str) -> list[Manga]:
         """Parse HTML and extract manga metadata."""
         soup = BeautifulSoup(html_content, "html.parser")
-        manga_list = []
 
-        for link_tag in soup.select("a[class*='hoverinfo_trigger fs14 fw-b']"):
-            try:
+        selector = "a[class*='fs14 fw-b']"
 
-        return manga_list
+        return [Manga.from_soup_tag(manga_item) for manga_item in soup.select(selector)]
